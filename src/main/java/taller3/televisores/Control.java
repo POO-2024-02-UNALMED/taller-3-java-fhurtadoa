8567e39c3acc4519516e26cbbbaa8e244f9cc097
fhurtadoa <fhurtadoa@unal.edu.co>
2024-11-18 00:00:03 -0500

    Código Televisor

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,77 @@
+package taller3.televisores;
+
+public class Control {
+    private TV tv;
+
+
+    public void enlazar(TV tv) {
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+
+    public TV getTv() {
+        return this.tv;
+    }
+
+
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
+
+
+    public void turnOn() {
+        if (this.tv != null) {
+            this.tv.turnOn();
+        }
+    }
+
+
+    public void turnOff() {
+        if (this.tv != null) {
+            this.tv.turnOff();
+        }
+    }
+
+
+    public void canalUp() {
+        if (this.tv != null) {
+            this.tv.canalUp();
+        }
+    }
+
+
+    public void canalDown() {
+        if (this.tv != null) {
+            this.tv.canalDown();
+        }
+    }
+
+
+    public void volumenUp() {
+        if (this.tv != null) {
+            this.tv.volumenUp();
+        }
+    }
+
+
+    public void volumenDown() {
+        if (this.tv != null) {
+            this.tv.volumenDown();
+        }
+    }
+
+
+    public void setCanal(int canal) {
+        if (this.tv != null) {
+            this.tv.setCanal(canal);
+        }
+    }
+
+    
+    public void setVolumen(int volumen) {
+        if (this.tv != null) {
+            this.tv.setVolumen(volumen);
+        }
+    }
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,127 @@
+package taller3.televisores;
+
+
+public class TV {
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+    private static int numTV;
+
+    
+    public TV(Marca marca, boolean estado){
+        this.marca = marca;
+        this.estado = estado;
+        numTV++;
+    }
+
+
+    public Marca getMarca(){
+        return this.marca;
+    }
+
+
+    public void setMarca(Marca marca){
+        this.marca = marca;
+    }
+
+
+    public int getCanal() {
+        return this.canal;
+    }
+
+
+    public void setCanal(int canal) {
+        if (this.estado && canal >= 1 && canal <= 120) {
+            this.canal = canal;
+        }
+    }
+
+
+    public int getPrecio() {
+        return this.precio;
+    }
+
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+
+    public int getVolumen() {
+        return this.volumen;
+    }
+
+
+    public void setVolumen(int volumen) {
+        if (this.estado && volumen >= 0 && volumen <= 7) {
+            this.volumen = volumen;
+        }
+    }
+
+
+    public Control getControl() {
+        return this.control;
+    }
+
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+
+    public static int getNumTV() {
+        return numTV;
+    }
+
+
+    public static void setNumTV(int num) {
+        numTV = num;
+    }
+
+
+    public void turnOn() {
+        this.estado = true;
+    }
+
+
+    public void turnOff() {
+        this.estado = false;
+    }
+
+
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+
+    public void canalUp() {
+        if (this.estado && this.canal < 120) {
+            this.canal++;
+        }
+    }
+
+
+    public void canalDown() {
+        if (this.estado && this.canal > 1) {
+            this.canal--;
+        }
+    }
+
+
+    public void volumenUp() {
+        if (this.estado && this.volumen < 7) {
+            this.volumen++;
+        }
+    }
+
+
+    public void volumenDown() {
+        if (this.estado && this.volumen > 0) {
+            this.volumen--;
+        }
+    }
+
+}
